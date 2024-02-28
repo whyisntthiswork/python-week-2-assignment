53b009af6a9d0ded52bd14065064f0cb01e24f0e
whyisntthiswork <fakinyi3@gmail.com>
2024-02-28 18:36:35 +0300

    initial commit

---
+++ week2python.py
@@ -0,0 +1,17 @@
+my_list = []
+print(my_list)
+my_list = [10, 20, 30, 40]
+print(my_list)
+my_list.insert(1, 15)
+print(my_list)
+my_list.extend([50, 60, 70])
+print(my_list)
+my_list.pop()
+print(my_list)
+my_list.sort()
+print(my_list)
+index_30 = my_list.index(30)
+print("index of 30 in my_list:", index_30)
+
+
+
